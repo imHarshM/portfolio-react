3e3cf83e95c920de5b8f5025ed95d5e9b84dcd43
Harshal Hari Mahajan <hmahajan@AIPL3648LT07474.local>
2021-03-15 19:29:10 +0530

    dd

--- src/components/porfolio/Menu.js
+++ src/components/porfolio/Menu.js
@@ -1,47 +1,83 @@
 import React from "react";
+import Scrollspy from "react-scrollspy";
+import useWindowDimensions from "../utils/WindowDimension";
 
 const Menu = ({ handleActive, activeMenu }) => {
+  const { width } = useWindowDimensions();
+
   return (
     <header className="header">
       <div className="top-menu">
-        <ul>
-          <li
-            className={activeMenu === "about" ? "active" : ""}
-            onClick={() => handleActive("about")}
+        {width > 1023 ? (
+          <ul>
+            <li
+              className={activeMenu === "about" ? "active" : ""}
+              onClick={() => handleActive("about")}
+            >
+              <a href="#/">
+                <i className="fa fa-user"></i>
+                <span className="link">About</span>
+              </a>
+            </li>
+            <li
+              className={activeMenu === "resume" ? "active" : ""}
+              onClick={() => handleActive("resume")}
+            >
+              <a href="#/">
+                <i className="fa fa-list-alt"></i>
+                <span className="link">Resume</span>
+              </a>
+            </li>
+            <li
+              className={activeMenu === "work" ? "active" : ""}
+              onClick={() => handleActive("work")}
+            >
+              <a href="#/">
+                <i className="fa fa-paint-brush"></i>
+                <span className="link">Work</span>
+              </a>
+            </li>
+            <li
+              className={activeMenu === "contact" ? "active" : ""}
+              onClick={() => handleActive("contact")}
+            >
+              <a href="#/">
+                <i className="fa fa-address-card"></i>
+                <span className="link">Contact</span>
+              </a>
+            </li>
+          </ul>
+        ) : (
+          <Scrollspy
+            items={["about-card", "resume-card", "work-card", "contact-card"]}
+            currentClassName="active"
           >
-            <a href="#/">
-              <i className="fa fa-user"></i>
-              <span className="link">About</span>
-            </a>
-          </li>
-          <li
-            className={activeMenu === "resume" ? "active" : ""}
-            onClick={() => handleActive("resume")}
-          >
-            <a href="#/">
-              <i className="fa fa-list-alt"></i>
-              <span className="link">Resume</span>
-            </a>
-          </li>
-          <li
-            className={activeMenu === "work" ? "active" : ""}
-            onClick={() => handleActive("work")}
-          >
-            <a href="#/">
-              <i className="fa fa-paint-brush"></i>
-              <span className="link">Work</span>
-            </a>
-          </li>
-          <li
-            className={activeMenu === "contact" ? "active" : ""}
-            onClick={() => handleActive("contact")}
-          >
-            <a href="#/">
-              <i className="fa fa-address-card"></i>
-              <span className="link">Contact</span>
-            </a>
-          </li>
-        </ul>
+            <li>
+              <a href="#about-card">
+                <i className="fa fa-user"></i>
+                <span className="link">About</span>
+              </a>
+            </li>
+            <li>
+              <a href="#resume-card">
+                <i className="fa fa-list-alt"></i>
+                <span className="link">Resume</span>
+              </a>
+            </li>
+            <li>
+              <a href="#work-card">
+                <i className="fa fa-paint-brush"></i>
+                <span className="link">Work</span>
+              </a>
+            </li>
+            <li>
+              <a href="#contact-card">
+                <i className="fa fa-address-card"></i>
+                <span className="link">Contact</span>
+              </a>
+            </li>
+          </Scrollspy>
+        )}
       </div>
     </header>
   );
